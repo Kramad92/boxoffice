439e86d8c85df7e25eb581f3a5735ad57ffe88f7
Kramad92 <kramad92@gmail.com>
2022-02-11 13:40:32 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Route } from 'react-router';
+import Navs from './components/navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <switch>
-      <Route exact path="/">
-        This is home page.
-      </Route>
-      <Route exact path="/starred">
-        This is starred
-      </Route>
-      <Route>
-        404 page not found
-      </Route>
-    </switch>
+    <div>
+      <Navs />
+      <switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route path="any">404 page not found</Route>
+      </switch>
+    </div>
   );
 }
 
