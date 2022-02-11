fbcc28494cebfdbbbeb0b9f8f7a05d18e631051e
Kramad92 <kramad92@gmail.com>
2022-02-11 15:58:34 +0100

    layout, title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router';
-import Navs from './components/navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route path="any">404 page not found</Route>
-      </switch>
-    </div>
+    <switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route path="any">404 page not found</Route>
+    </switch>
   );
 }
 
